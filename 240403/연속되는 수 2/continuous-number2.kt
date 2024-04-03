fc57d913713302552b5961765913c2542baa03e4
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-04-03 12:50:15 +0900

    Title: 연속되는 수 2, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240403/연속되는 수 2/continuous-number2.kt
@@ -0,0 +1,18 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val n: Int = sc.nextInt()
+    val numbers: List<Int> = List(n) { sc.nextInt() }
+    var curr = 0
+    var max = 0
+    numbers.forEachIndexed { index, number ->
+        if (index == 0 || numbers[index - 1] == numbers[index]) {
+            curr++
+        } else {
+            if (curr > max) max = curr
+            curr = 1
+        }
+    }
+    println(max)
+}
