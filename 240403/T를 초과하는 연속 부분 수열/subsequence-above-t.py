cd0dc1d797d95dd116db65e5fc914b87c76992e5
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-04-03 14:47:02 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

--- 240403/T를 초과하는 연속 부분 수열/subsequence-above-t.py
+++ 240403/T를 초과하는 연속 부분 수열/subsequence-above-t.py
@@ -15,30 +15,3 @@ for number in numbers:
     
 
 print(maxCount)
-
-
-# import java.util.*
-# import java.io.*
-
-# fun main() {
-#     val sc = Scanner(System.`in`)
-#     val input = sc.nextLine().split(" ")
-    
-#     val n = input[0].toInt()
-#     val t = input[1].toInt()
-#     val numbers = sc.nextLine().split(" ").map { it.toInt() }
-
-#     var count = 0
-#     var max = 0
-#     numbers.forEach {
-#         if (it > t) {
-#             count++
-#             if (count > max) max = count
-#         } else {
-#             if (count > max) max = count
-#             count = 0
-#         }
-#     }
-
-#     println(max)
-# }
