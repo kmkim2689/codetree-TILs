bdc34181497dc0e43cdcb62929eb5c275b08798a
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-04-08 15:20:33 +0900

    Title: 1이 3개 이상 있는 위치, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240408/1이 3개 이상 있는 위치/place-more-than-3-ones.kt
@@ -0,0 +1,56 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    var count = 0
+
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+    val grid = List(n) {
+        br.readLine().split(" ").map { it.toInt() }
+    }
+
+    val directions = listOf(
+        Position(1, 0),
+        Position(0, 1),
+        Position(0, -1),
+        Position(-1, 0)
+    )
+
+    for (i in 0 until n) {
+        for (j in 0 until n) {
+            val position = Position(i, j)
+            var curr = 0
+            directions.forEach {
+                val searchPosition = position + it
+                if (searchPosition.x in 0 until n
+                    && searchPosition.y in 0 until n
+                    && grid[searchPosition.x][searchPosition.y] == 1) {
+                    curr++
+                }
+            }
+            if (curr >= 3) count++
+        }
+    }
+    println(count)
+}
+
+data class Position(
+    val x: Int = 0,
+    val y: Int = 0
+) {
+    operator fun plus(other: Position): Position {
+        return Position(
+            this.x + other.x,
+            this.y + other.y
+        )
+    }
+}
+
+/*
+북 동 남 서 => 0 1 2 3
+Position(1, 0)
+Position(0, 1)
+Position(0, -1)
+Position(-1, 0)
+*/
