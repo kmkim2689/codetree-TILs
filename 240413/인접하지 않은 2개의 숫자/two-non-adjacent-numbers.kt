78c91b2dfcb789c84cecdb645c6693381735b66f
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-04-13 18:05:38 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 111ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 240413/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.kt
@@ -0,0 +1,18 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+    val numbers = br.readLine().trim().split(" ").map(String::toInt)
+    var maxValue = Int.MIN_VALUE
+
+    for (i in 0 until numbers.size - 2) {
+        for (j in i + 1 until numbers.size) {
+            val currTotal = numbers[i] + numbers[j]
+            if (currTotal > maxValue) maxValue = currTotal
+        }
+    }
+
+    println(maxValue)
+}
