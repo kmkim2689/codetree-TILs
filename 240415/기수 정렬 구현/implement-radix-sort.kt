6e1a0c2998b9d5e8c4bf6316ed12b4ec99dd44a7
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-04-15 23:12:44 +0900

    Title: 기수 정렬 구현, Time: 268ms, Memory: 19MB, Status: Wrong Answer - CodeTree

--- 240415/기수 정렬 구현/implement-radix-sort.kt
+++ 240415/기수 정렬 구현/implement-radix-sort.kt
@@ -2,16 +2,20 @@ val br = System.`in`.bufferedReader()
 
 fun main() = with(System.out.bufferedWriter()) {
     val n = br.readLine().toInt()
-    var numbers = br.readLine().trim().split(" ")
-    val maxLength = numbers.max()!!.length
-    numbers = numbers.map { it.padStart(maxLength, '0') }
-    for (pos in maxLength - 1 downTo 0) {
-        val arr = MutableList(10) { mutableListOf<String>() }
-        numbers.forEach {
-            arr[it[pos].toString().toInt()].add(it.toString())
-        }
-        numbers = arr.flatten()
+    val numbers = br.readLine().trim().split(" ").map(String::toInt)
+    var numbersByLength = numbers.groupBy { it.toString().length }
+    val numbersList = numbersByLength.map { (len, list) ->
+        list.toMutableList()
     }
-    numbers.forEach { write("${it.toInt()} ") }
+    println(numbersList)
+    // numbers = numbers.map { it.padStart(maxLength, '0') }
+    // for (pos in maxLength - 1 downTo 0) {
+    //     val arr = MutableList(10) { mutableListOf<String>() }
+    //     numbers.forEach {
+    //         arr[it[pos].toString().toInt()].add(it.toString())
+    //     }
+    //     numbers = arr.flatten()
+    // }
+    // numbers.forEach { write("${it.toInt()} ") }
     close()
 }
