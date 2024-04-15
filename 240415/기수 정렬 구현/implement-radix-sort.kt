4afd17a36063eb2a8b7e92c546e74db807f5aea0
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-04-15 23:02:51 +0900

    Title: 기수 정렬 구현, Time: 267ms, Memory: 20MB, Status: Wrong Answer - CodeTree

--- 240415/기수 정렬 구현/implement-radix-sort.kt
+++ 240415/기수 정렬 구현/implement-radix-sort.kt
@@ -2,14 +2,16 @@ val br = System.`in`.bufferedReader()
 
 fun main() = with(System.out.bufferedWriter()) {
     val n = br.readLine().toInt()
-    var numbers = br.readLine().trim().split(" ").map(String::toInt)
-    for (pos in numbers.first().toString().length - 1 downTo 0) {
-        val arr = MutableList(10) { mutableListOf<Int>() }
+    var numbers = br.readLine().trim().split(" ")
+    val maxLength = numbers.max()!!.length
+    numbers = numbers.map { it.padStart(maxLength, '0') }
+    for (pos in maxLength - 1 downTo 0) {
+        val arr = MutableList(10) { mutableListOf<String>() }
         numbers.forEach {
-            arr[it.toString()[pos].toString().toInt()].add(it)
+            arr[it[pos].toString().toInt()].add(it.toString())
         }
         numbers = arr.flatten()
     }
-    numbers.forEach { write("$it ") }
+    numbers.forEach { write("${it.toInt()} ") }
     close()
 }
