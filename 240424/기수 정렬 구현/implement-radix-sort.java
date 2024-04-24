5f79c73d4c6a3020caebd516cc787fd1e95104ab
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-04-24 15:39:52 +0900

    Title: 기수 정렬 구현, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240424/기수 정렬 구현/implement-radix-sort.java
+++ 240424/기수 정렬 구현/implement-radix-sort.java
@@ -9,7 +9,7 @@ public class Main {
             arr[i] = sc.nextInt();
         }
 
-        int maxNum = arr.max();
+        int maxNum = Arrays.stream(arr).max().getAsInt();
         int maxDigit = maxNum.toString().length(); // 4323 -> 4
 
         int exp = 1;
@@ -37,3 +37,45 @@ public class Main {
         }
     }
 }
+/*
+Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // 최대값 계산
+        int maxNum = Arrays.stream(arr).max().getAsInt();
+        // 최대 자리수 계산
+        int maxDigit = Integer.toString(maxNum).length();
+
+        int exp = 1;
+        ArrayList<Integer>[] bucket;
+
+        for(int i = 0; i < maxDigit; i++) {
+            bucket = new ArrayList[10];
+            for (int j = 0; j < 10; j++) {
+                bucket[j] = new ArrayList<Integer>();
+            }
+
+            for (int item : arr) {
+                int idx = (item / exp) % 10;
+                bucket[idx].add(item);
+            }
+
+            int idx = 0;
+            for (ArrayList<Integer> b : bucket) {
+                for (int item : b) {
+                    arr[idx++] = item;
+                }
+            }
+            exp *= 10;
+        }
+
+        for (int item : arr) {
+            System.out.print(item + " ");
+        }
+
+        sc.close();
+*/
