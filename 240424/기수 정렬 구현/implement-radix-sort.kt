68a3692034fe002b925e7aacae3f9ade37fb656e
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-04-24 15:01:02 +0900

    Title: 기수 정렬 구현, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240424/기수 정렬 구현/implement-radix-sort.kt
+++ 240424/기수 정렬 구현/implement-radix-sort.kt
@@ -5,38 +5,74 @@ fun main() {
     val n = sc.nextInt()
     var arr = IntArray(n) { sc.nextInt() }
 
-    val maxNum = arr.max()
-    val maxDigit = maxNum.toString().length - 1 // 3 자리 수 이면 2
+    // 최대값 찾기
+    val maxNum = arr.maxOrNull() ?: 0
+    // 최대 자리수 계산
+    val maxDigit = maxNum.toString().length
 
-    for(digit in 0 .. 6) { // digit: 자리수
-        // 나누는 수
-        var x = 10
-        // 새로 값을 저장할 리스트
-        val newList = List(10) { mutableListOf<Int>() }
+    var exp = 1
+    for (digit in 1..maxDigit) {
+        // 버킷 초기화
+        val bucket = Array(10) { mutableListOf<Int>() }
 
-        for(item in arr){
-            var nowDigit = ((item % x).toString().first() - '0').toInt()
-            newList[nowDigit].add(item)
+        // 현재 자리수에 따른 값 분배
+        for (item in arr) {
+            val index = (item / exp) % 10
+            bucket[index].add(item)
         }
 
-        val stored = mutableListOf<Int>()
-
-        for(i in 0 .. 9) {
-            for(j in 0 until newList[i].size) {
-                stored.add((newList[i])[j])
+        // 버킷에서 다시 배열로 수집
+        var index = 0
+        for (b in bucket) {
+            for (num in b) {
+                arr[index++] = num
             }
         }
-        x = x * 10
-        arr = stored.toIntArray()
-    }
-    for(item in arr){
-        print("${item} ")
+        exp *= 10
     }
 
+    // 결과 출력
+    println(arr.joinToString(" "))
 }
 
 
-/*
-6
-5 2 6 1 3 8
-*/
+// fun main() {
+//     val sc = Scanner(System.`in`)
+//     val n = sc.nextInt()
+//     var arr = IntArray(n) { sc.nextInt() }
+
+//     val maxNum = arr.max()
+//     val maxDigit = maxNum.toString().length - 1 // 3 자리 수 이면 2
+
+//     for(digit in 0 .. 6) { // digit: 자리수
+//         // 나누는 수
+//         var x = 10
+//         // 새로 값을 저장할 리스트
+//         val newList = List(10) { mutableListOf<Int>() }
+
+//         for(item in arr){
+//             var nowDigit = ((item % x).toString().first() - '0').toInt()
+//             newList[nowDigit].add(item)
+//         }
+
+//         val stored = mutableListOf<Int>()
+
+//         for(i in 0 .. 9) {
+//             for(j in 0 until newList[i].size) {
+//                 stored.add((newList[i])[j])
+//             }
+//         }
+//         x = x * 10
+//         arr = stored.toIntArray()
+//     }
+//     for(item in arr){
+//         print("${item} ")
+//     }
+
+// }
+
+
+// /*
+// 6
+// 5 2 6 1 3 8
+// */
