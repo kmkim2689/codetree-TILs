429e1bfc06b6d4aee0e7beae588fd26a994ac4d1
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-04-24 14:03:38 +0900

    Title: 기수 정렬 구현, Time: 562ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240424/기수 정렬 구현/implement-radix-sort.kt
@@ -0,0 +1,36 @@
+import java.util.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val n = sc.nextInt()
+    var arr = IntArray(n) { sc.nextInt() }
+
+    val maxNum = arr.max()
+    val maxDigit = maxNum.toString().length - 1 // 3 자리 수 이면 2
+
+    for(digit in maxDigit .. 0) { // digit: 자리수
+        // 나누는 수
+        var x = 10
+        // 새로 값을 저장할 리스트
+        val newList = List(10) { mutableListOf<Int>() }
+
+        for(item in arr){
+            val nowDigit = item % x
+            newList[nowDigit].add(item)
+        }
+
+        val stored = mutableListOf<Int>()
+
+        for(i in 0 .. 9) {
+            for(j in 0 until newList[i].size) {
+                stored.add((newList[i])[j])
+            }
+        }
+        x = x * 10
+        arr = stored.toIntArray()
+    }
+    for(item in arr){
+        print("${item} ")
+    }
+
+}
