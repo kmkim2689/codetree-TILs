2136ef9fa7034a1daa2a9fb6c7de347e2756e140
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-04-29 22:07:11 +0900

    Title: 힙 정렬 구현, Time: 1545ms, Memory: 52MB, Status: Passed - CodeTree

--- 240429/힙 정렬 구현/implement-heap-sort.java
+++ 240429/힙 정렬 구현/implement-heap-sort.java
@@ -3,32 +3,33 @@ import java.util.*;
 public class Main {
     static Scanner sc = new Scanner(System.in);
     static int n = sc.nextInt(); // 6
+    static int[] arr = new int[n+1];
+    
     public static void main(String[] args) {
-        int[] arr = new int[n+1];
         for(int i =1; i<=n; i++){
             arr[i] = sc.nextInt(); // 5 2 6 1 3 8
         }
 
-        heapSort(arr, n/2);
+        heapSort(n/2);
 
-        show(arr);
+        show();
     }
 
-    static void heapSort(int[] arr, int pos){
+    static void heapSort(int pos){
         // make maxHeap
         for(int i = pos; i >= 1; i--) {
-            heapify(arr, i, n);
+            heapify(i, n);
         }
 
         // sort
         for(int i = n; i >= 2; i--) {
-            swap(arr, 1, i);
-            heapify(arr, 1, i-1);
+            swap(1, i);
+            heapify(1, i-1);
         }
 
     }
 
-    static void heapify(int[] arr, int pos, int size) {
+    static void heapify(int pos, int size) {
         int maxIdx = pos;
         int lIdx = pos * 2;
         int rIdx = pos * 2 + 1;
@@ -42,18 +43,18 @@ public class Main {
         }
 
         if(maxIdx != pos) {
-            swap(arr, pos, maxIdx);
-            heapify(arr, maxIdx, size);
+            swap(pos, maxIdx);
+            heapify(maxIdx, size);
         }
     }
 
-    static void swap(int[] arr, int x, int y){
+    static void swap(int x, int y){
         int temp = arr[x];
         arr[x] = arr[y];
         arr[y] = temp;
     }
 
-    static void show(int[] arr) {
+    static void show() {
         for (int i = 1; i <= n; i++) {
             System.out.print(arr[i] + " ");
         }
