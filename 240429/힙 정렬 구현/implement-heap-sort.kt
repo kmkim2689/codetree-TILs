0bd1ad9269e6aeddcaff5891fa2f4b5fd4bedb67
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-04-29 21:47:07 +0900

    Title: 힙 정렬 구현, Time: 1652ms, Memory: 39MB, Status: Passed - CodeTree

--- 240429/힙 정렬 구현/implement-heap-sort.kt
+++ 240429/힙 정렬 구현/implement-heap-sort.kt
@@ -2,49 +2,47 @@ import java.util.*
 
 fun main() {
     val sc = Scanner(System.`in`)
-    val n = sc.nextInt() // 6
-    val arr = IntArray(n+1) { 0 } // 5 2 6 1 3 8
-    for(i in 1 .. n) {
-        arr[i] = sc.nextInt() 
+    val size = sc.nextInt() // size: 6
+    val arr = IntArray( size + 1 ) {0} // 5 2 6 1 3 8
+    for(i in 1 .. size) {
+        arr[i] = sc.nextInt()
     }
 
-    heapSort(arr, n)
+    // heapSort
+    heapSort(arr, size / 2)
 
-    for(i in 1 .. n) {
+    for(i in 1 .. size){
         print("${arr[i]} ")
     }
 }
 
-fun heapSort(arr: IntArray, n: Int) {
+fun heapSort(arr: IntArray, pos: Int) {
     // make max-heap
-    for(i in n / 2 downTo 1) {
-        heapify(arr, n, i)
+    for (i in pos downTo 1) {
+        heapify(arr, i, arr.size - 1)
     }
-
-    for (i in n downTo 2) {
+    
+    for(i in arr.size-1 downTo 2) {
         swap(arr, 1, i)
-        heapify(arr, i-1, 1)
+        heapify(arr, 1, i-1)
     }
 }
 
-// n: 크기. i: heapify 를 시작하는 인덱스
-fun heapify(arr: IntArray, n: Int, i: Int) {
-    var largestIdx = i
-    var l = 2 * i     // 왼쪽 자식 값
-    var r = 2 * i + 1 // 오른쪽 자식 값
+fun heapify(arr: IntArray, pos: Int, size: Int) {
+    var maxIdx = pos
+    val lIdx = pos * 2
+    val rIdx = pos * 2 + 1
 
-    // 왼쪽, 오른쪽 자식값이 크면 largestIdx 변경
-    if(l <= n && arr[l] > arr[largestIdx]) {
-        largestIdx = l
+    if (size >= lIdx && arr[lIdx] >= arr[maxIdx]) {
+        maxIdx = lIdx
     }
 
-    if(r <= n && arr[r] > arr[largestIdx]) {
-        largestIdx = r
+    if (size >= rIdx && arr[rIdx] >= arr[maxIdx]) {
+        maxIdx = rIdx
     }
-
-    if(largestIdx != i) {
-        swap(arr, i, largestIdx)
-        heapify(arr, n, largestIdx)
+    if(pos != maxIdx) {
+        swap(arr, pos, maxIdx)
+        heapify(arr, maxIdx, size)
     }
 }
 
