829f6523abe731f5a0d28c7c673e4e34d7c33717
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-05-02 00:33:46 +0900

    Title: 아름다운 수, Time: 129ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 240502/아름다운 수/beautiful-number.kt
@@ -0,0 +1,42 @@
+val numbers = arrayListOf<Int>()
+var count = 0
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val n = br.readLine().trim().toInt()
+    backtracking(1, n)
+    println(count)
+}
+
+fun backtracking(currSize: Int, maxSize: Int) {
+    if (currSize == maxSize + 1) {
+        if (isBeautiful()) count++
+        return
+    }
+
+    for (i in 1..4) {
+        numbers.add(i)
+        backtracking(currSize + 1, maxSize)
+        numbers.removeAt(numbers.size - 1)
+    }
+}
+
+fun isBeautiful(): Boolean {
+    var currNumber = numbers.first()
+    var currCount = 1
+    val newNumbers = numbers.toMutableList()
+    newNumbers.removeAt(0)
+    newNumbers.forEach { number ->
+        if (number != currNumber) {
+            if (currNumber % currCount != 0) {
+                return false
+            }
+            currNumber = number
+            currCount = 1
+        } else {
+            currCount++
+        }
+    }
+    val result = if (currCount % currNumber != 0) false else true
+    return result
+}
