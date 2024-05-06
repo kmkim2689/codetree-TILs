ea2e489d2abcf657455b32e00016420f43f19690
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-05-06 15:26:57 +0900

    Title: 겹치지 않게 선분 고르기, Time: 263ms, Memory: 22MB, Status: Wrong Answer - CodeTree

--- 240506/겹치지 않게 선분 고르기/select-segments-without-overlap.kt
+++ 240506/겹치지 않게 선분 고르기/select-segments-without-overlap.kt
@@ -1,7 +1,7 @@
 private val bw = System.out.bufferedWriter()
 private val curr = mutableListOf<Range>()
 private val ranges = arrayListOf<Range>()
-private var maxCount = 0
+private var maxCount = 1 // 선분이 하나라면 반드시 겹치지 않기 때문
 
 fun main() {
     val br = System.`in`.bufferedReader()
@@ -22,20 +22,22 @@ private fun backtracking(currSize: Int, maxSize: Int) {
     }
 
     ranges.forEach {
-        curr.add(it)
-        backtracking(currSize + 1, maxSize)
-        curr.removeAt(curr.size - 1)
+        if (it !in curr) {
+            curr.add(it)
+            backtracking(currSize + 1, maxSize)
+            curr.removeAt(curr.size - 1)
+        }
     }
 }
 
 private fun determine() {
     var isAvailable = true
-
     val sortedRanges = curr.sortedBy { it.start }
-    for (i in 0 until sortedRanges.size - 1) {
+    loop@for (i in 0 until sortedRanges.size - 1) {
         for (j in i until sortedRanges.size) {
             if (sortedRanges[i].end <= sortedRanges[j].start) {
-                return
+                isAvailable = false
+                break@loop
             }
         }
     }
