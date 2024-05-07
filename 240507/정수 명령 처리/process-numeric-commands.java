33a71a7944e5f057201a89e6b621d53f158ccd3e
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-05-07 14:39:10 +0900

    Title: 정수 명령 처리, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240507/정수 명령 처리/process-numeric-commands.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Stack stack = new Stack<Integer>();
+        for(int i = 0; i < n; i++) {
+            String cmd = sc.next();
+
+            switch(cmd) {
+                case "push": 
+                    int item = sc.nextInt();
+                    stack.push(item);
+                    break;
+                
+                case "pop":
+                    System.out.println(stack.pop());
+                    break;
+                
+                case "size":
+                    System.out.println(stack.size());
+                    break;
+                
+                case "empty":
+                    if(stack.isEmpty()){
+                        System.out.println(1);
+                    }else {
+                        System.out.println(0);
+                    }
+                
+                case "top" :
+                    System.out.println(stack.peek());
+                
+            }
+        }
+    }
+}
