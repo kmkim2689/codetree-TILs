4fd6688934455fbdad8777f1c382372377032274
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-05-08 00:50:48 +0900

    Title: 알파벳과 사칙연산, Time: 226ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 240508/알파벳과 사칙연산/calculations-with-alphabet.kt
@@ -0,0 +1,50 @@
+private val alphabets: MutableList<Char> = mutableListOf()
+private val operators = mutableListOf<Char>() 
+private val bw = System.out.bufferedWriter()
+private val numbers = mutableListOf<Int>()
+private var maxValue = 0
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    br.readLine().trim().forEach {
+        when (it) {
+            '-', '+', '*' -> operators.add(it)
+            else -> alphabets.add(it)
+        }
+    }
+    val maxSize = alphabets.distinct().size
+    backtracking(1, maxSize)
+    bw.write(maxValue.toString())
+    bw.close()
+}
+
+fun backtracking(currSize: Int, maxSize: Int) {
+    if (currSize == maxSize + 1) {
+        computeMaxValue()
+        return
+    } 
+
+    for (i in 1..4) {
+        numbers.add(i)
+        backtracking(currSize + 1, maxSize)
+        numbers.removeAt(numbers.size - 1)
+    }
+}
+
+fun computeMaxValue() {
+    val distinctChars = alphabets.distinct()
+    val dict = mutableMapOf<Char, Int>()
+    distinctChars.forEachIndexed { index, char ->
+        dict[char] = numbers[index]
+    }
+    var curr = dict[alphabets.first()]!!
+    operators.forEachIndexed { index, op ->
+        when (op) {
+            '+' -> curr = curr + dict[alphabets[index + 1].toChar()]!!
+            '-' -> curr = curr - dict[alphabets[index + 1].toChar()]!!
+            '*' -> curr = curr * dict[alphabets[index + 1].toChar()]!!
+        }
+    }
+
+    if (curr > maxValue) maxValue = curr
+}
