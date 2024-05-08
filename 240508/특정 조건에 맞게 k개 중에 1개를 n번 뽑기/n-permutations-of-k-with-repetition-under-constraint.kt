4c9598f5b80843677ebe44b110d600ce824d9241
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-05-08 13:07:01 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: 132ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 240508/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.kt
@@ -0,0 +1,30 @@
+val bw = System.out.bufferedWriter()
+val curr = mutableListOf<Int>()
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val (k, n) = br.readLine().split(" ").map(String::toInt)
+    backtracking(1, n, k)
+    bw.close()
+}
+
+fun backtracking(
+    currSize: Int,
+    maxSize: Int,
+    maxNumber: Int
+) {
+    if (currSize == maxSize + 1) {
+        curr.forEach {
+            bw.write("$it\n")
+        }
+        return
+    }
+
+    for (i in 1..maxNumber) {
+        if (curr.size < 2 || (curr[curr.size - 1] == i && curr[curr.size - 2] == i) == false) {
+            curr.add(i)
+            backtracking(currSize + 1, maxSize, maxNumber)
+            curr.removeAt(curr.size - 1)
+        }
+    }
+}
