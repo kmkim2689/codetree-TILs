ddf50a1fbbc556749c807f14c3aab0874e1038e9
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-05-12 21:31:22 +0900

    Title: 1차원 윷놀이, Time: 127ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 240512/1차원 윷놀이/yutnori-1d.kt
@@ -0,0 +1,32 @@
+val bw = System.out.bufferedWriter()
+private lateinit var numbers: List<Int>
+private val indexes = arrayListOf<Int>()
+private var maxScore = Int.MIN_VALUE
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val (n, m, k) = br.readLine().trim().split(" ").map(String::toInt)
+    numbers = br.readLine().trim().split(" ").map(String::toInt)
+    
+    backtracking(1, n, k, m)
+    bw.write(maxScore.toString())
+    bw.close()
+}
+
+fun backtracking(currSize: Int, maxSize: Int, numSize: Int, target: Int) {
+    if (currSize == maxSize + 1) {
+        val scores = MutableList(maxSize) { 0 }
+        indexes.zip(numbers) { index, number ->
+            scores[index] += number
+        }
+        val total = scores.count { it >= target }
+        if (maxScore < total) maxScore = total
+        return
+    }
+
+    for (i in 0 until maxSize) {
+        indexes.add(i)
+        backtracking(currSize + 1, maxSize, numSize, target)
+        indexes.removeAt(indexes.size - 1)
+    }
+}
