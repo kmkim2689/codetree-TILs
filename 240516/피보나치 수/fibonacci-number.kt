9b8d82619e3ba2bbec41f889a6176f70853984c6
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-05-16 23:41:56 +0900

    Title: 피보나치 수, Time: 216ms, Memory: 10MB, Status: Passed - CodeTree

--- 240516/피보나치 수/fibonacci-number.kt
+++ 240516/피보나치 수/fibonacci-number.kt
@@ -1,4 +1,5 @@
 val bw = System.out.bufferedWriter()
+val memo: MutableList<Int> = MutableList(46) { -1 }
 
 fun main() {
     val br = System.`in`.bufferedReader()
@@ -8,6 +9,11 @@ fun main() {
 }
 
 fun fib(number: Int): Int {
-    if (number == 1 || number == 2) return 1
-    return fib(number - 1) + fib(number - 2)
+    if (number == 1 || number == 2) {
+        memo[number] = 1
+        return memo[number]
+    }
+    if (memo[number] != -1) return memo[number]
+    memo[number] = fib(number - 1) + fib(number - 2)
+    return memo[number]
 }
