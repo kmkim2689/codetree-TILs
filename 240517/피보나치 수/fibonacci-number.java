ef3cc99578c335b5f84287777644a9b51f128ae4
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-05-17 09:56:04 +0900

    Title: 피보나치 수, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240517/피보나치 수/fibonacci-number.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    static int n = sc.nextInt();
+    static int[] arr = new int[n];
+    
+    public static void main(String[] args) {
+        for(int i = 0; i < n; i ++) {
+            arr[i] = -1;
+        }
+        System.out.println(pibo(n));
+    }
+
+    static int pibo(int i) {
+        if(i == 1) return 1;
+        if(i == 2) return 1;
+
+        if(arr[i-2] == -1) {
+            arr[i-2] = pibo(i-2);
+        }
+        if(arr[i-1] == pibo(i-1)){
+            arr[i-1] = pibo(i-1);
+        }
+
+        return arr[i-2] + arr[i-1];
+    }
+}
