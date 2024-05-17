ce492aa0a7ef1df7f19abbbf92cd74251d0b9934
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-05-17 09:48:36 +0900

    Title: 피보나치 수, Time: 183ms, Memory: 10MB, Status: Passed - CodeTree

--- 240517/피보나치 수/fibonacci-number.kt
+++ 240517/피보나치 수/fibonacci-number.kt
@@ -3,6 +3,7 @@ import java.util.*
 val sc = Scanner(System.`in`)
 val n = sc.nextInt()
 var ans = 1
+val arr = IntArray(n) { -1 }
 
 fun main(){
     println(pibo(n))
@@ -12,5 +13,12 @@ fun pibo(i: Int): Int {
     if(i == 1) return 1
     if(i == 2) return 1
 
-    return pibo(i-2) + pibo(i-1)
+    if(arr[i - 2]== -1){
+        arr[i-2] = pibo(i-2)
+    }
+
+    if(arr[i-1] == -1) {
+        arr[i-1] = pibo(i-1)
+    }
+    return arr[i-2] + arr[i-1]
 }
