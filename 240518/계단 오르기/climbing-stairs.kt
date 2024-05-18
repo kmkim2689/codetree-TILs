53337e659a224ab5ff33e5528acc341cebfd573f
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-05-18 22:37:43 +0900

    Title: 계단 오르기, Time: 114ms, Memory: 10MB, Status: Passed - CodeTree

--- 240518/계단 오르기/climbing-stairs.kt
+++ 240518/계단 오르기/climbing-stairs.kt
@@ -1,43 +1,16 @@
 val bw = System.out.bufferedWriter()
-private lateinit var result: MutableList<Int>
 
 fun main() {
     val br = System.`in`.bufferedReader()
     val n = br.readLine().trim().toInt()
-    result = MutableList(n + 1) { -1 }
-    val result = solve(n)
-    bw.write("${result % 10007}")
+    val result: Array<Int> = Array(1001) { -1 }
+    result[0] = 1 // 안 올라가는 방법은 한 가지
+    result[1] = 0
+    result[2] = 1
+    result[3] = 1
+    for (i in 4..n) {
+        result[i] = (result[i - 2] + result[i - 3]) % 10007
+    }
+    bw.write("${result[n]}")
     bw.close()
-}
-
-fun solve(height: Int): Int {
-    if (height == 2 || height == 3 || height == 4) {
-        result[height] = 1
-        return result[height]
-    }
-    if (result[height] != -1) {
-        return result[height]
-    }
-
-    val first: Int = if (height - 2 <= 1) {
-        0
-    } else if ((height - 2) in 2..4) {
-        1
-    } else {
-        result[height - 2] = solve(height - 4) + solve(height - 5)
-        result[height - 2]
-    }
-
-    val second: Int = if (height - 3 <= 1) {
-        0
-    } else if ((height - 3) in 2..4) {
-        1
-    } else {
-        result[height - 3] = solve(height - 5) + solve(height - 6)
-        result[height - 3]
-    }
-
-
-    result[height] = first + second
-    return result[height]
 }
