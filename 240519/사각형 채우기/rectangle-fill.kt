4b49b94547ba143e1141ffdd1dfdf5e2fa3e5ead
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-05-19 23:29:07 +0900

    Title: 사각형 채우기, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240519/사각형 채우기/rectangle-fill.kt
@@ -0,0 +1,17 @@
+val bw = System.out.bufferedWriter()
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val n = br.readLine().trim().toInt()
+
+    val answer = MutableList(n + 1) { -1 }
+    answer[0] = 0
+    answer[1] = 1
+    answer[2] = 2
+    answer[3] = 3
+    for (i in 4..n) {
+        answer[i] = answer[i - 1] + answer[i - 2]
+    }
+    bw.write((answer[n] % 10007).toString())
+    bw.close()
+}
