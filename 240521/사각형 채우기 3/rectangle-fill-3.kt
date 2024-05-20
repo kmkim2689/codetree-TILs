9cb39b61653fb46898a9b1ea331e3b987d242dcd
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-05-21 00:09:26 +0900

    Title: 사각형 채우기 3, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240521/사각형 채우기 3/rectangle-fill-3.kt
@@ -0,0 +1,24 @@
+import java.util.*
+
+val sc = Scanner(System.`in`)
+val n = sc.nextInt()
+val tab = IntArray(n + 1) { 0 }
+
+fun main() {
+    if(n == 1){
+        println(2)
+        return
+    }
+    if(n == 2) {
+        println(7)
+        return
+    }
+    tab[0] = 1
+    tab[1] = 2
+    tab[2] = 7
+
+    for(i in 3 .. n) {
+        tab[i] = ( tab[i-3] * 2 + tab[i - 2] * 3 + tab[i-1] * 2 ) % 1_000_000_007
+    }
+    println(tab[n])
+}
