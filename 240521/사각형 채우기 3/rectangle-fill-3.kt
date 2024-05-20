b8594df368d3583fc3e3d8ac38c3329f998cdf2b
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-05-21 00:59:45 +0900

    Title: 사각형 채우기 3, Time: 144ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240521/사각형 채우기 3/rectangle-fill-3.kt
+++ 240521/사각형 채우기 3/rectangle-fill-3.kt
@@ -14,17 +14,13 @@ fun main() {
         println(7)
         return
     }
-    if( n == 3) {
-        println(22)
-        return
-    }
 
     tab[0] = 1
     tab[1] = 2
     tab[2] = 7
     for (i in 3 .. n) {
         tab[i] = ((2 * tab[i-1] + 3 * tab[i-2])) % MOD
-        for(j in 3 .. i step -1){
+        for(j in 3 .. i){
             tab[i] += (2 * tab[i-j]) % MOD
         }
     }
