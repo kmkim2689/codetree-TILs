8799fcd0297ed6590cd0ace4e1cfc996f8830c00
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-05-22 10:58:49 +0900

    Title: 사각형 채우기 2, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240522/사각형 채우기 2/rectangle-fill-2.kt
@@ -0,0 +1,27 @@
+import java.util.*
+
+val sc = Scanner(System.`in`)
+val n = sc.nextInt()
+val MOD = 10_007
+
+fun main() {
+    if( n == 1 ) {
+        println(1)
+        return
+    }
+    if( n == 2 ) {
+        println(3)
+        return
+    }
+
+    val dp = IntArray(n+1) { 0 }
+    dp[0] = 1
+    dp[1] = 1
+    dp[2] = 3
+
+    for(i in 2 .. n) {
+        dp[i] = (dp[i-1] + dp[i-2] * 2) % MOD
+    }
+
+    println(dp[n])
+}
