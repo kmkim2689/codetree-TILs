14244c55c6e71cb3be6d7221155c8a6effb1f9b0
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-05-23 09:37:43 +0900

    Title: xor 결과 최대 만들기, Time: 3077ms, Memory: 19MB, Status: Time Limit Exceed - CodeTree

---
+++ 240523/xor 결과 최대 만들기/max-of-xor.kt
@@ -0,0 +1,31 @@
+val bw = System.out.bufferedWriter()
+var maxValue = Int.MIN_VALUE
+lateinit var numbers: List<Int>
+var curr = mutableListOf<Int>()
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val (n, m) = br.readLine().trim().split(" ").map(String::toInt)
+    numbers = br.readLine().trim().split(" ").map(String::toInt)
+    backtracking(1, m)
+    bw.write(maxValue.toString())
+    bw.close()
+}
+
+fun backtracking(currSize: Int, maxSize: Int) {
+    if (currSize == maxSize + 1) {
+        val total = curr.reduce { total, num ->
+            total xor num
+        }
+        if (total > maxValue) maxValue = total
+        return
+    }
+
+    numbers.forEach {
+        if (it !in curr) {
+            curr.add(it)
+            backtracking(currSize + 1, maxSize)
+            curr.removeAt(curr.size - 1)
+        }
+    }
+}
