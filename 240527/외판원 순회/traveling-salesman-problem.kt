8a22683fdb51fee944e7dd1603ceecc7a9d693c2
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-05-27 14:15:45 +0900

    Title: 외판원 순회, Time: 139ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 240527/외판원 순회/traveling-salesman-problem.kt
@@ -0,0 +1,39 @@
+val bw = System.out.bufferedWriter()
+private lateinit var grid: List<List<Int>>
+private val currNumbers = mutableListOf<Int>(0)
+private var minValue = Int.MAX_VALUE
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val n = br.readLine().trim().toInt()
+    grid = List(n) {
+        br.readLine().trim().split(" ").map(String::toInt)
+    }
+    backtracking(2, n)
+    bw.write(minValue.toString())
+    bw.close()
+}
+
+fun backtracking(curr: Int, max: Int) {
+    if (curr == max + 1) {
+        var currValue = 0
+        var row = 0
+        currNumbers.forEachIndexed { i, number ->
+            if (i != 0) {
+                currValue += grid[row][currNumbers[i]]
+                row = currNumbers[i]
+            }
+        }
+        currValue += grid[row][0]
+        if (currValue < minValue) minValue = currValue
+        return
+    }
+
+    for (i in 1 until max) {
+        if (i !in currNumbers) {
+            currNumbers.add(i)
+            backtracking(curr + 1, max)
+            currNumbers.removeAt(currNumbers.size - 1)
+        }
+    }
+}
