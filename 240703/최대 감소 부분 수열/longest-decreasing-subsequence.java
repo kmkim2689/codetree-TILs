2f023c537a88050e29f74bd6c27853622ca7961b
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-07-03 20:05:32 +0900

    Title: 최대 감소 부분 수열, Time: 213ms, Memory: 12MB, Status: Wrong Answer - CodeTree

--- 240703/최대 감소 부분 수열/longest-decreasing-subsequence.java
+++ 240703/최대 감소 부분 수열/longest-decreasing-subsequence.java
@@ -18,26 +18,19 @@ public class Main {
         for(int i = 2; i <= n; i++){
             int maxDp = 0;
             for(int j = 1; j < i; j++) {
-                if(seq[i] > seq[j]) {
-                    maxDp = Math.max(maxDp, dp[j]);
+                if(seq[i] < seq[j]) {
+                    maxDp = Math.max(maxDp, dp[j] + 1);
                 }
             }
-            dp[i] = maxDp + 1;
+            dp[i] = maxDp;
         }
 
         int result = 1;
         for(int i = 1; i <= n; i++) {
             result = Math.max(result, dp[i]);
         }
+
         
         System.out.println(result);
     }
 }
-
-/*
-4
-  4 2 3 1 // seq
-0 1 2 1 3 // dp
-
-
-*/
