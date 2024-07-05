2197b881fb511b7dcaf9513fdc9089c6242915b9
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-07-05 09:06:15 +0900

    Title: 최대 점프 횟수, Time: 207ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 240705/최대 점프 횟수/maximum-number-of-jumps.kt
@@ -0,0 +1,28 @@
+import java.util.*
+import kotlin.math.*
+
+val sc = Scanner(System.`in`)
+val n = sc.nextInt()
+val seq = IntArray(n) { sc.nextInt() }
+val dp = IntArray(n) { -1 }
+
+fun main() {    
+
+    dp[0] = 0
+    
+    for(i in 1 .. n-1) {
+        for(j in 0 .. i-1){
+            if(j + seq[j] >= i) {
+                dp[i] = max(dp[i], dp[j] + 1)
+            }
+        }
+    }
+
+    var result = 0
+
+    for(i in 1 .. n-1) {
+        result = max(result, dp[i])
+    }
+
+    println(result)
+}
