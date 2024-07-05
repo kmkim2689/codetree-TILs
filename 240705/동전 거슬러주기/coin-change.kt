f7c7a939ffaacd74ad9e2772f6348309cd4090fe
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-07-05 09:57:57 +0900

    Title: 동전 거슬러주기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240705/동전 거슬러주기/coin-change.kt
@@ -0,0 +1,26 @@
+import java.util.*
+import kotlin.math.*
+
+val sc = Scanner(System.`in`)
+val n = sc.nextInt()
+val m = sc.nextInt()
+
+val coins = IntArray(n) { sc.nextInt() }
+val dp = IntArray(m + 1) {Int.MAX_VALUE}
+
+fun main() {
+
+    dp[0] = 0
+
+    for (i in 1 .. m) {
+        for(j in 0 .. n-1) {
+            if(i >= coins[j]) {
+                if(dp[i - coins[j]] == Int.MAX_VALUE) continue
+
+                dp[i] = min(dp[i], dp[i - coins[j]] + 1)
+            }
+        }
+    }
+
+    println(dp[m])
+}
