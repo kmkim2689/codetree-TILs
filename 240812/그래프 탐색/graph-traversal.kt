20100bae295eec1126868eedd579916ea13e1c9b
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-08-12 20:53:12 +0900

    Title: 그래프 탐색, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240812/그래프 탐색/graph-traversal.kt
+++ 240812/그래프 탐색/graph-traversal.kt
@@ -1,12 +1,28 @@
 private lateinit var grid: MutableList<MutableList<Int>>
+private var count = 0
+private lateinit var visited: MutableList<Boolean>
 
 fun main() {
     val br = System.`in`.bufferedReader()
     val (n, m) = br.readLine().split(" ").map(String::toInt)
-    grid = MutableList(m) { mutableListOf<Int>() }
+    grid = MutableList(n + 1) { MutableList(n + 1) { 0 } }
+    visited = MutableList(n) { false }
     repeat(m) {
         val (x, y) = br.readLine().split(" ").map(String::toInt)
-        grid[x].add(y)
+        grid[x][y] = 1
+        grid[y][x] = 1
     }
-    println(grid)
+    dfs(1)
+
+    println(count - 1)
+}
+
+fun dfs(vertex: Int) {
+    visited[vertex] = true
+    count++
+    for (i in 0 until grid.size) {
+        if (grid[vertex][i] == 1 && !visited[i]) {
+            dfs(i)
+        }
+    }
 }
