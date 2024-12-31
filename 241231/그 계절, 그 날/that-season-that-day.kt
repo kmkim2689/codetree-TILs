ac29ae74c6a53af085f26f7dd4bc11749c90fa93
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-12-31 22:08:24 +0900

    Title: 그 계절, 그 날, Time: 130ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 241231/그 계절, 그 날/that-season-that-day.kt
@@ -0,0 +1,39 @@
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val bw = System.out.bufferedWriter()
+
+    val (y, m, d) = br.readLine().trim().split(" ").map(String::toInt)
+    var special = false
+    if (y % 4 == 0) {
+        if (y % 100 == 0 && y % 400 == 0) {
+            special = true
+        } else if (y % 100 != 0) {
+            special = true
+        }
+    }
+
+    var result = when (m) {
+        3, 4, 5 -> "Spring"
+        6, 7, 8 -> "Summer"
+        9, 10, 11 -> "Fall"
+        12, 1, 2 -> "Winter"
+        else -> throw error("invalid month")
+    }
+
+    when (m) {
+        2 -> {
+            if (special) {
+                if (m > 29) result = "-1"
+            } else {
+                if (m > 28) result = "-1"
+            }
+        }
+        4, 6, 9, 11 -> {
+            if (m > 30) result = "-1"
+        }
+    }
+
+    bw.write(result)
+    bw.flush()
+    bw.close()
+}
