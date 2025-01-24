f9bb9c8a24ce3b5bb9257fa18df5504f3a839219
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2025-01-24 20:30:18 +0900

    Title: 쪼개어 배낭 채우기 구현, Time: 565ms, Memory: 27MB, Status: Passed - CodeTree

--- 250124/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.kt
+++ 250124/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.kt
@@ -1,16 +1,31 @@
+import java.util.PriorityQueue
+
 fun main() {
     val br = System.`in`.bufferedReader()
     val bw = System.out.bufferedWriter()
 
     val (n, m) = br.readLine().trim().split(" ").map(String::toInt)
-    val jewList = List(n) {
+
+    val jewList = PriorityQueue<Pair<Int, Int>>() { o1, o2 ->
+        val t1 = (o1.second).toDouble() / (o1.first).toDouble()
+        val t2 = (o2.second).toDouble() / (o2.first).toDouble()
+
+        if (t1 == t2) {
+            o1.first - o2.first
+        } else {
+            if (t2 > t1) 1 else -1
+        }
+    }
+
+    repeat(n) {
         val (w, v) = br.readLine().trim().split(" ").map(String::toInt)
-        w to v
-    }.sortedByDescending { (it.second / it.first).toDouble() }
+        jewList.add(w to v)
+    }
 
     var leftW = m
     var sum: Double = 0.0
-    for (jew in jewList) {
+    while (jewList.isNotEmpty()) {
+        val jew = jewList.poll()
         val weight = jew.first
         val value = jew.second
         if (leftW >= weight) {
@@ -18,7 +33,7 @@ fun main() {
             leftW -= weight
         } else {
             val availableFraction = (leftW.toDouble() / weight.toDouble())
-            sum += (value * availableFraction)
+            sum += (value * availableFraction).toDouble()
             break
         }
     }
